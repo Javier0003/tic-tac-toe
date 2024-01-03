2493b28674f1346077a86f28fc84e6db208aea4e
Javier0003 <jpere0500@gmail.com>
2024-01-03 10:00:10 -0400

    component for the grid

---
+++ src/components/grid/grid.tsx
@@ -0,0 +1,50 @@
+import styles from './styles.module.css'
+
+interface GridProps {
+  grid: string[][]
+  handleClick: (y: number, x: number) => void
+  theme: boolean
+}
+
+const Grid: React.FC<GridProps> = ({ grid, handleClick, theme }) => {
+  return (
+    <div className={styles.grid}>
+      {grid.map((row, y) => (
+        <div className={styles.row} key={`${y}_row`}>
+          {row.map((rowItem, x) => {
+            let icon: string[]
+            switch (rowItem) {
+              case 'X':
+                icon = ['src/icons/lightX.png', 'src/icons/darkX.png']
+                break
+              case 'O':
+                icon = ['src/icons/oazul.png', 'src/icons/oblancaa.png']
+                break
+              default:
+                icon = ['', '']
+                break
+            }
+            return (
+              <div
+                className={theme ? styles.darkRowItem : styles.rowItem}
+                key={`${x}_rowItem`}
+                onClick={() => handleClick(y, x)}
+              >
+                {icon[0] ? (
+                  <img
+                    src={theme ? icon[1] : icon[0]}
+                    className={styles.icon}
+                  />
+                ) : (
+                  ''
+                )}
+              </div>
+            )
+          })}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Grid
